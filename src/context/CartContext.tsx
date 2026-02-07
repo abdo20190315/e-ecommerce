4e31f3b571c433003699100f7c7175ae56c5960b
abdo20190315 <saayeed119@gmail.com>
2026-02-06 19:57:44 -0500

    Integrate CartContext for managing cart state across components, update Navbar to display cart items, enhance Cart page with item listing and order summary, and implement loading state handling; add CartResponse type definitions for improved type safety.

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,29 +1,48 @@
 'use client'
+import { CartResponse } from "@/types";
 import {ReactNode, createContext, useEffect,  useState} from "react";
 
 
 
- const cartContext=   createContext({})
+ export const cartContext=   createContext<{
+  cartData: CartResponse | null;
+  setCartData: (value:CartResponse | null) => void;
+  isLoading: boolean;
+  setIsloading: (value:boolean) => void;
+  getCart:()=>void;
+ }>({//init value
+    cartData:null,
+    setCartData:()=>{},
+    isLoading:false,
+    setIsloading:()=>{},
+    getCart:()=>{}
 
- export default function cartContextProvider({children}:{children:ReactNode}){
-    const [carData, setCartData] = useState(null)
+ })
+
+ export default function CartContextProvider({children}:{children:ReactNode}){
+
+    const [isloading, setIsloading] = useState(false)
+    const [cartData, setCartData] = useState<CartResponse | null>(null)
 
    async function getCart(){
+    setIsloading(true)
         const response = await fetch(`https://ecommerce.routemisr.com/api/v1/cart`,{
             headers:{
                 token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5ODJhMmFhYmFiODkzZmViMzY1NDQ2MiIsIm5hbWUiOiJBYmRlbHJhaG1hbiIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzcwMzAzMDU3LCJleHAiOjE3NzgwNzkwNTd9.zeH3KYRGFLWbK5iRJzPr4Bg_y7D8tfmWn58WF9Un7NA"
             }
         })
-        const data = await response.json()
+        const data:CartResponse = await response.json()
+        setCartData(data)
         console.log(data);
+        setIsloading(false)
         
     }
-    useEffect(() => {}, [
+    useEffect(() => {
         getCart()
-    ])
+    }, [])
     
 
-return <cartContext.Provider value={{}}>
+return <cartContext.Provider value={{ cartData ,setCartData , isloading , setIsloading , getCart}}>
     {children}
 </cartContext.Provider>
 
