da67a58a4221bbc8a1629d706b868a1a11a62fab
abdo20190315 <saayeed119@gmail.com>
2026-02-15 05:03:35 -0500

    Add comment in addToCart action file for clarity; no functional changes made.

--- src/app/product/_action/addToCart.action.ts
+++ src/app/product/_action/addToCart.action.ts
@@ -7,7 +7,7 @@ export async function addToCart(productId: string) {
     const accessToken = await DecodeToken();  
 
 
-
+//test
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/cart`, {
         method: 'POST',
         body: JSON.stringify({ productId }),
